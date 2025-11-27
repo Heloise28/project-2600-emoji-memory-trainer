b07132e2d3b8d638cfa5f4009ba1377efb06d81f
Heloise28 <cestlaprune@gmail.com>
2025-11-26 18:32:46 -0800

    updated server.js for deployment (tell server to serve the files)

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5001;
+const path = require('path'); // first step for deployment (for deploy and tell server to server the static file (html, css, js))
 
 // Middleware
 app.use(cors());
@@ -27,6 +28,25 @@ app.use('/api/next_user_id', nextUserIdRouter);
 
 const loginRouter = require('./routes/login');
 app.use('/api/login', loginRouter);
+
+
+// second step for deployment:
+// Production setup for serving the React frontend
+if (process.env.NODE_ENV === 'production') {
+    
+    // 2. Serve static files from the client's build directory
+    // NOTE: If your client build output is 'build', change 'dist' to 'build'
+    // If you are using Vite (as you mentioned earlier): Vite's default build command 
+    // is vite build, and it nearly always outputs to a folder named dist 
+    // (short for distribution).
+    app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+
+    // 3. For any other request, send the main index.html file
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'client', 'dist', 'index.html'));
+    });
+}
+
 
 
 // Start the server
