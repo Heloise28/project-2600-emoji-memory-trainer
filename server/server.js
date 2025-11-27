efa1e1309b80edb9980fa834b3506c8be54ff0fb
Heloise28 <cestlaprune@gmail.com>
2025-11-26 22:21:02 -0800

    same problem, another try

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.use('/api/login', loginRouter);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
-    app.get('/:path(*)', (req, res) => {
+    app.get('/{*splat}', (req, res) => {
         res.sendFile(path.resolve(__dirname, '..', 'client', 'dist', 'index.html'));
     });
 }
